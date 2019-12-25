03e61d8d3a33fd591fca582e7a3c00d62639cc03
Joel Truher <truher@google.com>
2019-12-24 18:35:16 -0800

    basic AFR works

--- env/EngineEnv.py
+++ env/EngineEnv.py
@@ -8,10 +8,13 @@ import random
 AIR_DENSITY = 1.225 # mg/cc
 CYL_VOLUME = 400    # cc
 AFR_TARGET = 14.7
+FAR_TARGET = 1.0/14.7
 MIN_MAP = 0.3
 MAX_MAP = 1.0 # atm
 MIN_AFR = 8.0
 MAX_AFR = 22.0
+MIN_FAR = 1.0/22.0
+MAX_FAR = 1.0/8.0
 LEAN_MISFIRE = 18.0
 RICH_MISFIRE = 9.0
 
@@ -43,16 +46,31 @@ class EngineEnv(gym.Env):
         self.reward_range = (-float('inf'), 0.0)
         # state
         self.map = MIN_MAP
-        self.afr = MAX_AFR
-        # PPO wants symmetric action space, starts at 0 with stddev 1, i.e. [-1,1]
+        #self.afr = MAX_AFR
+        self.far = MAX_FAR
+
+        # PPO
+        self.action_space = gym.spaces.Box(low=-1.0, high=1.0, shape=(1,), dtype=np.float32)
+
+        # A2C
+        #self.action_space = gym.spaces.Box(low=0.0, high=1.0, shape=(1,), dtype=np.float32)
+
         # but negative action really makes no sense, so try [0,1] decigrams
         # action: squirt in dg[0,1]
         #self.action_space = gym.spaces.Box(low=0.0, high=1.0, shape=(1,), dtype=np.float32)
-        self.action_space = gym.spaces.Box(low=0.0, high=100.0, shape=(1,), dtype=np.float32)
-        # observation: afr[8,22]
-        self.observation_space = gym.spaces.Box(np.array([8]), np.array([22], dtype=np.float32))
-        self.steps = 0
 
+        # SAC
+        # max fuel at VE=100%, afr 11 is 44 mg.  min fuel at 30% MAP, afr 18 is 8
+        #self.action_space = gym.spaces.Box(low=5.0, high=50.0, shape=(1,), dtype=np.float32)
+        #self.action_space = gym.spaces.Box(low=-1.0, high=1.0, shape=(1,), dtype=np.float32)
+
+        # observation: map[0,1], afr[8,22]
+        #self.observation_space = gym.spaces.Box(np.array([0.0,8.0]), np.array([1.0,22.0], dtype=np.float32))
+        #self.observation_space = gym.spaces.Box(np.array([MIN_MAP, MIN_FAR]), np.array([MAX_MAP, MAX_FAR], dtype=np.float32))
+        # observation is map, 1/afr - 1/afr_target
+        self.observation_space = gym.spaces.Box(np.array([MIN_MAP, -0.03]), np.array([MAX_MAP, 0.06], dtype=np.float32))
+
+        self.steps = 0
 
         #self.state = np.array([0, 0, 0]) # position, velocity, acceleration
         # action: force[-10, 10]
@@ -63,22 +81,66 @@ class EngineEnv(gym.Env):
 
         #self.steps = 0
 
+    def observation(self):
+        #return np.array([self.map, self.afr])
+        # make afr symmetric
+        return np.array([self.map, (self.far - FAR_TARGET)])
+
+    # agent acts on the *previous* observation to get a reward, so change the state at the end
     def step(self, action):
         air_mass = self.map * CYL_VOLUME * AIR_DENSITY # mg
+        #fuel_mass = 25.0 * (action[0] + 1.0) # mg
+        fuel_mass = 25 + 25.0 * action[0]   # mg
         #fuel_mass = action[0] * 100.0 # mg
-        fuel_mass = action[0] # mg
-        self.afr = min(max(air_mass/(fuel_mass+1.0), MIN_AFR), MAX_AFR)
+        #fuel_mass = action[0] # mg
+        #self.afr = min(max(air_mass/(fuel_mass+1.0), MIN_AFR), MAX_AFR)
+        self.far = min(max(fuel_mass/air_mass, MIN_FAR), MAX_FAR)
 
         self.steps += 1
-        #done = (self.steps > 10)
-        done = True # one step episode
+        done = (self.steps % 10 == 0)
+        #done = True
         # MAP random walk
         #if (self.steps % 10 == 0):
         #    self.map = min(max(self.map + random.uniform(-0.2, 0.2), MIN_MAP), MAX_MAP)
-        reward = 0.0 - ((self.afr - AFR_TARGET)**2)
-        #if (self.steps % 10 == 0):
-        print(f'map:{self.map:8.3f} air_mass:{air_mass:8.3f} fuel_mass:{fuel_mass:8.3f} afr:{self.afr:8.3f} rew:{reward:8.3f}')
-        return np.array([self.afr]), reward, done, {}
+        #reward = 0.0 - ((self.afr - AFR_TARGET)**2)
+        #reward = 0.0 - (abs(self.afr - AFR_TARGET))
+
+        # help it to find the right AFR
+        #predicted_afr = air_mass/(fuel_mass+1.0)
+        predicted_far = fuel_mass/air_mass
+        #reward = 0.0 - ((self.afr - AFR_TARGET)**2) - ((predicted_afr - AFR_TARGET)**2)
+        #reward = 0.0 - 100.0 * ((self.far - FAR_TARGET)**2) - 100.0 * ((predicted_far - FAR_TARGET)**2)
+        #reward = 0.0 - 100.0 * (abs(self.far - FAR_TARGET)) - 100.0 * (abs(predicted_far - FAR_TARGET))
+        #reward = 0.0 - 1000.0 * ((self.far - FAR_TARGET)**2)
+        #reward = 0.0 - 10000.0 * abs(self.far - FAR_TARGET)
+        #reward = max(0.0 - 10000.0 * abs(self.far - FAR_TARGET), -200) # clamp the reward
+        #reward = 0.0 - 10000.0 * ((self.far - FAR_TARGET)**2)
+        reward = max(0.0 - 10000.0 * ((self.far - FAR_TARGET)**2), -5) # clamp the reward
+
+
+
+        if (self.steps % 100 == 0):
+        #if (self.steps % 1 == 0):
+            afr = 1/(self.far + 0.000001)
+            afr_err = afr - AFR_TARGET
+            far_err = self.far - FAR_TARGET
+            print(f'step:{self.steps:8d} action:{action[0]:8.3f} map:{self.map:8.3f} air_mass:{air_mass:8.3f} fuel_mass:{fuel_mass:8.3f} far:{self.far:8.4f} far_err:{far_err:8.4f} afr:{afr:8.3f} afr_err:{afr_err:8.3f} rew:{reward:8.4f}')
+
+        # maybe change throttle position
+        #if (self.steps % 100 == 0):
+            #if self.map < 0.5:
+            #    self.map = MAX_MAP
+            #else:
+            #    self.map = MIN_MAP
+            #self.map = min(max(self.map + random.uniform(-0.2, 0.2), MIN_MAP), MAX_MAP)
+        # scale, shape
+        map_dot = (-1 if (self.steps % 2 == 0) else 1) * random.weibullvariate(0.05,1.0)
+        #print(f'map_dot:{map_dot:8.3f}')
+
+        self.map = min(max(self.map + map_dot, MIN_MAP), MAX_MAP)
+
+
+        return self.observation(), reward, done, {}
 
 
 
@@ -105,8 +167,8 @@ class EngineEnv(gym.Env):
 
     # reset the episode but not the engine
     def reset(self):
-        self.steps = 0
-        return np.array([self.afr])
+        #self.steps = 0
+        return self.observation()
 
         #self.state = np.array([self.np_random.uniform(low=-10.0, high=10.0), 0, 0]) # position, velocity, accel
         #return np.array([self.state[0], self.state[1], self.state[2], 0])
--- main.py
+++ main.py
@@ -1,40 +1,60 @@
 import gym
 
-#from stable_baselines.common.policies import MlpPolicy
-from stable_baselines.sac.policies import MlpPolicy
-from stable_baselines.sac.policies import FeedForwardPolicy
 from stable_baselines.common.vec_env import DummyVecEnv
-#from stable_baselines import PPO2
-from stable_baselines import SAC
+#from stable_baselines.common.vec_env import VecNormalize
+
+# SAC
+#from stable_baselines.sac.policies import FeedForwardPolicy
+#from stable_baselines.sac.policies import MlpPolicy
+#from stable_baselines import SAC
+
+# PPO
+from stable_baselines.common.policies import MlpPolicy
+from stable_baselines import PPO2
+
+# A2C
+#from stable_baselines.common.policies import MlpPolicy
+#from stable_baselines import A2C
+
 from env.EngineEnv import EngineEnv
 
 def printit(x,y):
-    print(f'{x[0]:10.3f} {y:10.3f}')
+    map = x[0]
+    far_err = x[1]
+    far = far_err + 1.0/14.7
+    afr = 1/far
+    print(f'{x[0]:10.3f} {x[1]:10.5f} {afr:10.3f} {y:10.4f}')
 
 # maybe less network will be less random?
-class CustomSACPolicy(FeedForwardPolicy):
-    def __init__(self, *args, **kwargs):
-        super(CustomSACPolicy, self).__init__(*args, **kwargs,
-                                           layers=[128, 128, 128],
-                                           layer_norm=False,
-                                           feature_extraction="mlp")
+#class CustomSACPolicy(FeedForwardPolicy):
+#    def __init__(self, *args, **kwargs):
+#        super(CustomSACPolicy, self).__init__(*args, **kwargs,
+#                                           layers=[128, 128, 128],
+#                                           layer_norm=False,
+#                                           feature_extraction="mlp")
 
 def main():
     env = DummyVecEnv([lambda: EngineEnv()])
+    #env = VecNormalize(env, norm_obs=True, norm_reward=False, clip_obs=10.)
+    #env = VecNormalize(env)
     env.env_method("seed", 0)
-    #model = PPO2(MlpPolicy, env, tensorboard_log="/tmp/foo")
-    model = SAC(MlpPolicy, env, tensorboard_log="/tmp/foo")
+    # more value function loss
+    model = PPO2(MlpPolicy, env, vf_coef=10.0, tensorboard_log="/tmp/foo")
+    #model = A2C(MlpPolicy, env, tensorboard_log="/tmp/foo")
+    #model = SAC(MlpPolicy, env, tensorboard_log="/tmp/foo")
     #model = SAC(CustomSACPolicy, env, tensorboard_log="/tmp/foo")
     model.learn(total_timesteps=1000000)
+    #model.learn(total_timesteps=400000)
+    #model.learn(total_timesteps=500000)
     obs = env.reset()
-    print('afr        reward')
-    #      1234567890 1
+    print('map        far_err    afr        reward')
+    #      1234567890 1234567890 1234567890 1
     for i in range(2000):
         action, _states = model.predict(obs)
         obs, rewards, done, info = env.step(action)
-        if done:
-            printit(info[0]['terminal_observation'], rewards[0])
-            print("")
+        #if done:
+        #    printit(info[0]['terminal_observation'], rewards[0])
+        #    print("")
         printit(obs[0], rewards[0])
 
 if __name__ == '__main__':
